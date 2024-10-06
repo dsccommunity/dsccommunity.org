3f1f9b9234c517053293086dd489c4313b4f7960
Johan Ljunggren <johlju@users.noreply.github.com>
2024-10-06 08:13:24 +0000

    Fix devcontainer

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo '
+hugo version
+
+echo
+echo -en "\033[1;32mStart local server using:\033[0m "
+if [ "$CODESPACES" = "true" ]; then
+    echo "hugo server --baseURL https://$CODESPACE_NAME-1313.app.github.dev --appendPort=false"
+else
+    echo "hugo server"
+fi
+echo
+' >> ~/.bashrc
